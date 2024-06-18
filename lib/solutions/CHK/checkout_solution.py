d65f4a8b07f8f2c795d50ac408e953423ee90b51
sbx_user1051 <sbx_user1051@169.254.53.205>
2024-06-18 08:59:59 +0000

    Tue Jun 18 08:59:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -8,5 +8,14 @@ def checkout(skus):
 
     counter = Counter(skus.split(","))
 
-    raise NotImplementedError()
+    # 3A is 130
+    total_a = (counter["A"] // 3) * 130 + (counter["A"] % 3) * table["A"]
+
+    # 2B is 45
+    total_b = (counter["B"] // 2) * 45 + (counter["B"] % 2) * table["B"]
+
+    total = total_a + total_b + counter["C"] * table["C"] + counter["D"] * table["D"]
+
+    return total
+
 
---
+++ test/solution_tests/CHK/test_chk.py
@@ -0,0 +1,6 @@
+from solutions.CHK import sum_solution
+
+
+class TestCheckout:
+    def test_chk(self):
+        assert sum_solution.compute(1, 2) == 3
