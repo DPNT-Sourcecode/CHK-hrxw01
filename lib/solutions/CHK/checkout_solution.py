eee81e79769685d2aef9cca10f9adfd73d38d088
sbx_user1051 <sbx_user1051@169.254.53.205>
2024-06-18 08:56:59 +0000

    Tue Jun 18 08:56:59 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,6 +1,12 @@
+from collections import Counter
 
 
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
+    table = {"A": 50, "B": 30, "C": 20, "D": 15}
+
+    counter = Counter(skus.split(","))
+
     raise NotImplementedError()
+
